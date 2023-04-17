ae757d0feec6e5effd4fdcce00266a0baa2a86cb
= <=>
2023-04-18 00:36:56 +0700

    First commit

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+  <title>Pendaftaran Calon Ketua BAZNAS TUBA</title>
+  <link href="{{ asset('assets') }}/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="{{ asset('assets') }}/css/offcanvas.css" rel="stylesheet">
+</head>
+
+<body class="bg-light">
+
+
+
+   <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="{{ asset('assets') }}/img/banner/BANNER-965X250.png" alt="First slide">
+        </div>
+      </div>
+    </div>
+
+  <main role="main" class="container">
+    <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-primary rounded box-shadow">
+      <img class="mr-3" src="{{ asset('assets') }}/img/logo/baznas-tuba.png" alt="" width="55" height="55">
+      <div class="lh-100">
+        <h6 class="mb-0 text-white lh-100">BAZNAS</h6>
+        <small>Kabupaten Tulang Bawang</small>
+      </div>
+    </div>
+
+    <div class="my-3 p-3 bg-white rounded box-shadow">
+      <h6 class="border-bottom border-gray pb-2 mb-3">Persyaratan Umum Pendaftaran Calon Pimpinan BAZNAS Kab. Tulang Bawang</h6>
+      <div id="accordion">
+         <div class="card">
+           <div class="card-header" id="headingTwo">
+             <h5 class="mb-0">
+               <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                 Ketentuan Umum
+               </button>
+             </h5>
+           </div>
+           <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+             <div class="card-body">
+               <ul class="list-group">
+                  <li class="list-group-item">1. Penglolan Zakat adalah kegiatan perencanaan, pelaksanaan, dan pengkoordinasian, dalam pengumpulan, pendistribusin, dan pendayagunaan zakat</li>
+                  <li class="list-group-item">Dapibus ac facilisis in</li>
+                  <li class="list-group-item">Morbi leo risus</li>
+                  <li class="list-group-item">Porta ac consectetur ac</li>
+                  <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+             </div>
+           </div>
+         </div>
+         <div class="card">
+           <div class="card-header" id="headingThree">
+             <h5 class="mb-0">
+               <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                 Persyratan Umum
+               </button>
+             </h5>
+           </div>
+           <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+             <div class="card-body">
+               Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+             </div>
+           </div>
+         </div>
+         <div class="card">
+           <div class="card-header" id="headingThree">
+             <h5 class="mb-0">
+               <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                 Tata Cara Pendaftaran
+               </button>
+             </h5>
+           </div>
+           <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+             <div class="card-body">
+               Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+             </div>
+           </div>
+         </div>
+       </div>
+    </div>
+
+    <div class="my-3 p-3 bg-white rounded box-shadow">
+      <h6 class="border-bottom border-gray pb-2 mb-0">Formulir Pendaftaran</h6>
+      <form action="">
+         <div class="form-group mt-3">
+            <label for="name">Nama Lengkap</label>
+            <input type="text" class="form-control" id="name" name="name">
+          </div>
+         <div class="form-group mt-3">
+            <label for="name">Email</label>
+            <input type="text" class="form-control" id="name" name="name">
+          </div>
+         <div class="form-group mt-3">
+            <label for="name">No Tlp</label>
+            <input type="text" class="form-control" id="name" name="name">
+          </div>
+      </form>
+    </div>
+  </main>
+
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+  <script src="../../../../assets/js/vendor/popper.min.js"></script>
+  <script src="{{ asset('assets') }}/js/bootstrap.min.js"></script>
+  <script src="{{ asset('assets') }}/js/vendor/holder.min.js"></script>
+  <script src="{{ asset('assets') }}/js/offcanvas.js"></script>
+</body>
+
+</html>
