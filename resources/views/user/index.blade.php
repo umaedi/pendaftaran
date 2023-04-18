f8db87d0afb283877f09a84be384cc3e30f100f7
= <=>
2023-04-18 11:09:31 +0700

    04/18/23

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -14,9 +14,6 @@
 </head>
 
 <body class="bg-light">
-
-
-
    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
@@ -38,36 +35,49 @@
       <h6 class="border-bottom border-gray pb-2 mb-3">Persyaratan Umum Pendaftaran Calon Pimpinan BAZNAS Kab. Tulang Bawang</h6>
       <div id="accordion">
          <div class="card">
-           <div class="card-header" id="headingTwo">
+           <div class="card-header" id="headingOne">
              <h5 class="mb-0">
-               <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+               <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                  Ketentuan Umum
                </button>
              </h5>
            </div>
-           <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+           <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
              <div class="card-body">
                <ul class="list-group">
                   <li class="list-group-item">1. Penglolan Zakat adalah kegiatan perencanaan, pelaksanaan, dan pengkoordinasian, dalam pengumpulan, pendistribusin, dan pendayagunaan zakat</li>
-                  <li class="list-group-item">Dapibus ac facilisis in</li>
-                  <li class="list-group-item">Morbi leo risus</li>
-                  <li class="list-group-item">Porta ac consectetur ac</li>
-                  <li class="list-group-item">Vestibulum at eros</li>
+                  <li class="list-group-item">2. Badan Amil Zakat Nasional yang selanjutnya disebut BAZNAS adalah lembaga yang melakukan pengelolaan zakat secara nasional</li>
+                  <li class="list-group-item">3. BAZNAS Kabupaten Tulang Bawang adalah lembaga yang melaksanakan tugas dan fungsi BAZNAS di kab. Tulang Bawang.</li>
+                  <li class="list-group-item">4. Pimpinan BAZNAS Kabupaten Tulang Bawang adalah ketua dan oleh wakil ketua BAZNAS Kabupaten Tulang Bawangyang di angkat oleh bupati setelah mendapatkan pertimbangan dari BAZNAS, terdiri dari 1 (satu) orang ketua dan 4 (empat) orang wakil ketua yang berasal dari unsur masyarakat yang meliputi ulama, tenaga profesional dan tokoh msyarakat Islam.</li>
+                  <li class="list-group-item">5. Pantia seleksi adalah panitia yang melaksanakan proses seleksi calon Pimpinan BAZNAS Kabupaten Tulang Bawang.</li>
                 </ul>
              </div>
            </div>
          </div>
          <div class="card">
-           <div class="card-header" id="headingThree">
+           <div class="card-header" id="headingTwo">
              <h5 class="mb-0">
-               <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+               <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                  Persyratan Umum
                </button>
              </h5>
            </div>
-           <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+           <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
              <div class="card-body">
-               Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+              <ul class="list-group">
+                <li class="list-group-item">1. Warga Negara Indonesia</li>
+                <li class="list-group-item">2. Bergama Islam</li>
+                <li class="list-group-item">3. Bertakwa Kepada Allah SWT.</li>
+                <li class="list-group-item">4. Berakhlak mulia.</li>
+                <li class="list-group-item">5. Berusia minimal 40 (empat puluh) tahun.</li>
+                <li class="list-group-item">6. Sehat jasmani dan rohani.</li>
+                <li class="list-group-item">7. Tidak menjadi anggota partai politik.</li>
+                <li class="list-group-item">8. Tidak terlinat dalam kegiatan politik praktis.</li>
+                <li class="list-group-item">9. Memiliki kompetensi di bidang pengelolaan zakat.</li>
+                <li class="list-group-item">10 Bersedia bekerja penuh waktu.</li>
+                <li class="list-group-item">11. Tidak pernah dihukum karena melakukan tindak pidana kejahatan yang diancam dengan pidana penjara paling singkat 5 (lima) tahun; dan.</li>
+                <li class="list-group-item">12. Tidak merangkap jabatan sebagai pengurus dan/atau pegawai pengelola zakat lainya.</li>
+              </ul>
              </div>
            </div>
          </div>
@@ -81,7 +91,20 @@
            </div>
            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
              <div class="card-body">
-               Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+              <ul class="list-group">
+                <li class="list-group-item">1. Pendaftaran Calon Pimpinan BAZNAS Kabupaten Tulang Bawang dilakukan dengan mengajukan lamaran secara tertulis kepada Panitia Seleksi (format surat lamaran terlampir) dengan melapirkan dokumen:</li>
+                <li class="list-group-item">a. Daftar Riwayat Hidup</li>
+                <li class="list-group-item">3. Bertakwa Kepada Allah SWT.</li>
+                <li class="list-group-item">4. Berakhlak mulia.</li>
+                <li class="list-group-item">5. Berusia minimal 40 (empat puluh) tahun.</li>
+                <li class="list-group-item">6. Sehat jasmani dan rohani.</li>
+                <li class="list-group-item">7. Tidak menjadi anggota partai politik.</li>
+                <li class="list-group-item">8. Tidak terlinat dalam kegiatan politik praktis.</li>
+                <li class="list-group-item">9. Memiliki kompetensi di bidang pengelolaan zakat.</li>
+                <li class="list-group-item">10 Bersedia bekerja penuh waktu.</li>
+                <li class="list-group-item">11. Tidak pernah dihukum karena melakukan tindak pidana kejahatan yang diancam dengan pidana penjara paling singkat 5 (lima) tahun; dan.</li>
+                <li class="list-group-item">12. Tidak merangkap jabatan sebagai pengurus dan/atau pegawai pengelola zakat lainya.</li>
+              </ul>
              </div>
            </div>
          </div>
@@ -113,8 +136,6 @@
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
     crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-  <script src="../../../../assets/js/vendor/popper.min.js"></script>
   <script src="{{ asset('assets') }}/js/bootstrap.min.js"></script>
   <script src="{{ asset('assets') }}/js/vendor/holder.min.js"></script>
   <script src="{{ asset('assets') }}/js/offcanvas.js"></script>
