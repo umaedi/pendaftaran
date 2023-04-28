1c3b4aa26fb7080e4a7674318b5c6dc218be4d0b
= <=>
2023-04-28 09:13:43 +0700

    afsdfds

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
   <meta name="theme-color" content="#0a5414"/>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ $title ?? "Dashboard" }}</title>
-  <link rel="stylesheet" href="{{ asset('css') }}/bootstrap.4.3.1.css">
+  <link rel="stylesheet" href="{{ asset('assets/css') }}/bootstrap.4.3.1.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
   <!-- Template CSS -->
